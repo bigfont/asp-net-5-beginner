1190f7a3c22acad81f53cf2cbad4768445eae565
Shaun Luttin <admin@shaunluttin.com>
2016-02-22 08:00:37 -0800

    Add a Core on Azure Web App sample.

---
+++ App06CoreOnAzure/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace App06 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello from a minimal ASP.NET Core rc1 Web App.");
+            });
+        }
+    }
+}
