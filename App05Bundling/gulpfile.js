5f8cd3ca09d0d23fbd13fb53a3bd7b4db39fdd8f
Anthony Chu <anthony@anthonychu.ca>
2016-02-21 23:27:23 -0800

    Create bundles for js and for css

--- App05Bundling/gulpfile.js
+++ App05Bundling/gulpfile.js
@@ -5,31 +5,35 @@ var gulp = require("gulp"),
   rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify");
+  uglify = require("gulp-uglify"),
+  copy = require("gulp-copy");
 
 var webroot = "./wwwroot/";
 
 var paths = {
-  js: webroot + "js/**/*.js",
+  js: [
+    webroot + "lib/jquery/dist/jquery.min.js",
+    webroot + "lib/bootstrap/dist/js/bootstrap.js",
+    webroot + "js/**/*.js"
+  ],
   minJs: webroot + "js/**/*.min.js",
-  css: webroot + "css/**/*.css",
+  css: [
+    webroot + "lib/bootstrap/dist/css/bootstrap.css",
+    webroot + "css/**/*.css"
+  ],
   minCss: webroot + "css/**/*.min.css",
-  concatJsDest: webroot + "js/site.min.js",
-  concatCssDest: webroot + "css/site.min.css"
+  concatJsDest: webroot + "bundles/js/site-bundle.min.js",
+  concatCssDest: webroot + "bundles/css/site-bundle.min.css",
+  fonts: [webroot + "lib/bootstrap/dist/fonts/*"],
+  fontsDestFolder: webroot + "bundles/fonts/"
 };
 
-gulp.task("clean:js", function (cb) {
-  rimraf(paths.concatJsDest, cb);
+gulp.task("clean", function (cb) {
+  rimraf(webroot + "bundles", cb);
 });
-
-gulp.task("clean:css", function (cb) {
-  rimraf(paths.concatCssDest, cb);
-});
-
-gulp.task("clean", ["clean:js", "clean:css"]);
 
 gulp.task("min:js", function () {
-  return gulp.src([paths.js, "!" + paths.minJs], {
+  return gulp.src(paths.js.concat(["!" + paths.minJs]), {
     base: "."
   })
     .pipe(concat(paths.concatJsDest))
@@ -38,10 +42,15 @@ gulp.task("min:js", function () {
 });
 
 gulp.task("min:css", function () {
-  return gulp.src([paths.css, "!" + paths.minCss])
+  return gulp.src(paths.css.concat(["!" + paths.minCss]))
     .pipe(concat(paths.concatCssDest))
     .pipe(cssmin())
     .pipe(gulp.dest("."));
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task("min:fonts", function () {
+  return gulp.src(paths.fonts)
+    .pipe(copy(paths.fontsDestFolder, { prefix: 5 }));
+})
+
+gulp.task("min", ["min:js", "min:css", "min:fonts"]);
