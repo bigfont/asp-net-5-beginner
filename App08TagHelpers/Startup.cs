d6e53cd96f87e159ecd2f2cc6431902e576d4f66
Anthony Chu <anthony@anthonychu.ca>
2016-02-25 23:54:26 -0800

    Add giphy tag helper

--- App08TagHelpers/Startup.cs
+++ App08TagHelpers/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using GiphyTagHelper;
 
 namespace App08TagHelpers
 {
@@ -14,7 +15,6 @@ namespace App08TagHelpers
     {
         public Startup(IHostingEnvironment env)
         {
-            // Set up configuration sources.
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables();
@@ -22,15 +22,13 @@ namespace App08TagHelpers
         }
 
         public IConfigurationRoot Configuration { get; set; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
+            services.AddGiphyTagHelper(new GiphyTagHelperConfiguration(Configuration["giphy:apiKey"]));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
@@ -57,7 +55,6 @@ namespace App08TagHelpers
             });
         }
 
-        // Entry point for the application.
         public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
     }
 }
